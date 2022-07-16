d7fd7181c80467f8ccb499bf46828c9b8acdd1e0
jainamshah512 <jainamshah512@gmail.com>
2022-07-17 00:33:09 +0530

    Learning from JavaTpoint

---
+++ helloWorld.py
@@ -0,0 +1,224 @@
+# print("Hello World")
+#
+# def my_fun():
+#     pass
+
+# print("Hello World", "My name is jainam", "Dhasdas", end="\np")
+# print("Dswdas")
+
+# a = 10
+# print("a =",a)
+
+# name = input("Enter name: ")
+# print(name)
+
+# num1 = int(input("Enter Number: "))
+# print(num1)
+
+# list1 = [1, 2, 3, 4, "hello", "python", [23, 234, 4.23], {12, 34, 556}]
+# print(len(list1))
+#
+# print(type(list1))
+#
+# tuple1 = (23, 34.3445, "jaina,", "dasfdsfs")
+# print(type(tuple1))
+#
+# tuple1[2] = 23
+#
+# print(tuple1)
+
+# str1 = "Hello Python"
+# str2 = "Namas'te"
+# str3 = 'This will print "'
+#
+# print(str1, str2, str3, sep="\n")
+# print("Hello world")
+#
+# print(len(str1))
+
+# student = {"Name": "Jainam", "marks": 98, "grade": "A"}
+# print(student["marks"])
+#
+# print(type(student))
+#
+# set1 = {"jan", "feb", "jan", "march", "april"}
+# print(type(set1))
+
+# print(dir())
+
+# a = 10
+# print(dir())
+
+# x = 10
+
+# print("helloworld"
+#
+# )
+
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+#
+# for i in list1:
+#     if i==5:
+#         continue
+#     print(i)
+
+
+#
+# # Function that takes input from user and prints sum of integers
+#
+# numbers = []
+#
+# for i in range(1, 11):
+#     number = int(input("Enter {} number: ".format(i)))
+#     numbers.append(number)
+#
+# print(numbers)
+# print(sum(numbers))
+
+
+"""This string will
+server purpose of multiline comments
+I love Python"""
+
+#
+# _name = "Jainam"
+# __marks = 100
+#
+# print(dir())
+# na0me = 10
+
+# var = 10
+# Var = 20
+#
+# print(var, Var)
+
+# a = 10
+# b = 10
+#
+# c = d = 100
+# print(id(a), id(b))
+#
+# print(id(c), id(d))
+#
+# a, b, c = 100, 2343.34, 342
+#
+# print(a, b, c)
+
+# def my_fun():
+#     a = 190
+#     b = 200
+#     c = a + b
+#     return c
+#
+# print(my_fun())
+#
+# print(c)
+#
+# x = 10
+# def my_fun():
+#     global x
+#     x = 15
+#     return x
+#
+# print(my_fun())
+#
+# print(x)
+
+
+# a = 100
+# del a
+#
+# print(a)
+#
+# a  = 1000**10000
+# print(type(a))
+
+# a = 1023.343
+# b = "Hello world"
+# c = [34, 34]
+#
+# print(type(a), type(b), type(c))
+
+# a = True
+# print(type(a))
+
+# print(isinstance(2323.343, float))
+
+# complex_num = 23 + 234j
+# print(type(complex_num))
+# print(isinstance(complex_num, complex))
+
+# a = -334
+# b = 334
+# print(type(a))
+# print(id(a), id(b))
+
+# complex_num = 10 + 30j
+# print(complex_num)
+
+# a = 100
+# print(a.imag)
+#
+# '''This will be served
+# as multiline comment'''
+# """This will be served
+# as multiline comment"""
+#
+# str1 = "Hello " + "Python"
+# print(str1)
+
+# print("JAinam " * 10)
+
+# str1 = "Hello World! This is Jainam."
+#
+# print(str1[1:5])
+# print(str1[::-2])
+
+# list1 = [233, 45, 343, 656, 67, "adas"]
+# print(list1[::__import__()])
+
+# tup1 = (23, 456, 57, 343, 67, "dasda")
+# tup2 = ("Hy", "dasdas", 3434)
+
+# print(tup1 + tup2)
+# tup2[2] = 'A'
+
+# dict1 = {1: "hello", 2: "jainam", 3: "Python"}
+# print(dict1[2])
+# print(dict1.keys(), dict1.values())
+
+# list1 = (1, 2, 3)
+# print(hash(list1))
+
+# a = True
+# print(type(a))
+
+# print(False)
+
+# a = 0
+# print(bool(a))
+
+# set1 = {"hello, ", 1, 23, 345, 54345}
+# for i in set1:
+#     print(i)
+
+# set2 = set()
+# print(type(set2))
+
+# set1.add("jainam")
+# print(set1)
+
+# set1.remove(565)
+# print(set1)
+
+# import keyword
+#
+# print(len(keyword.kwlist))
+
+# print(help("in"))
+
+# help("SEQUENCEMETHODS")
+
+# str1 = ''
+# print(bool(str1))
+
